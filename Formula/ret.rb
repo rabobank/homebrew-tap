47082a46d496d3d24b50645c19a6dfdf9bbc928f
jreleaserbot <jreleaser@kordamp.org>
2023-05-25 12:09:13 +0000

    ret 0.1.2

--- Formula/ret.rb
+++ Formula/ret.rb
@@ -1,10 +1,10 @@
-# Generated with JReleaser 1.6.0 at 2023-05-22T07:35:31.737657188Z
+# Generated with JReleaser 1.6.0 at 2023-05-25T12:04:48.141052216Z
 class Ret < Formula
   desc "RET Engineering Tools"
   homepage "https://github.com/rabobank/ret-engineering-tools"
-  url "https://github.com/rabobank/ret-engineering-tools/releases/download/0.1.1/ret-0.1.1-osx-x86_64.zip"
-  version "0.1.1"
-  sha256 "fcdb33baa5131a19c17dec8ebebb2689d3ce914a63b806f1e3896b6b591b821d"
+  url "https://github.com/rabobank/ret-engineering-tools/releases/download/0.1.2/ret-0.1.2-osx-x86_64.zip"
+  version "0.1.2"
+  sha256 "913a0d54f5407cd470c39106e769d04b3b0dccd8325cb1d2ad601336d8361164"
   license "&#39;MIT License&#39;"
 
 
@@ -15,6 +15,6 @@ class Ret < Formula
 
   test do
     output = shell_output("#{bin}/ret --version")
-    assert_match "0.1.1", output
+    assert_match "0.1.2", output
   end
 end
