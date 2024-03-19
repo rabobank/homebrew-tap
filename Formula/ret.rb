5b0defc6d6dc298c673f55f6bdb296cdf7042fe8
jreleaserbot <jreleaser@kordamp.org>
2024-03-19 10:41:24 +0000

    ret 0.2.3

--- Formula/ret.rb
+++ Formula/ret.rb
@@ -1,12 +1,23 @@
-# Generated with JReleaser 1.11.0 at 2024-03-18T11:55:59.359880507Z
+# Generated with JReleaser 1.11.0 at 2024-03-19T10:39:08.300878235Z
 
 class Ret < Formula
   desc "RET Engineering Tools"
   homepage "https://github.com/rabobank/ret-engineering-tools"
-  url "https://github.com/rabobank/ret-engineering-tools/releases/download/0.2.2/ret-0.2.2-osx-x86_64.zip"
-  version "0.2.2"
-  sha256 "58139dfc2169b1c2c4c9300eb390c8715f129dac5905fed2db2175a444f5e1c8"
+  version "0.2.3"
   license "MIT License"
+
+  if OS.linux? && Hardware::CPU.intel?
+    url "https://github.com/rabobank/ret-engineering-tools/releases/download/0.2.3/ret-0.2.3-linux-x86_64.zip"
+    sha256 "0b0732252fd3486d1e0d87e03052b4bcd001a06ffc84084c6ea53e51f7fdaa2d"
+  end
+  if OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/rabobank/ret-engineering-tools/releases/download/0.2.3/ret-0.2.3-osx-aarch_64.zip"
+    sha256 "c9244ce8f4beb7dd328c3e66b8de4756249d3da44d54afc0961370e700a8cb60"
+  end
+  if OS.mac? && Hardware::CPU.intel?
+    url "https://github.com/rabobank/ret-engineering-tools/releases/download/0.2.3/ret-0.2.3-osx-x86_64.zip"
+    sha256 "a0961d3d2e0654b2256c38df21fc7b3d7e6af210a4c35184cfe2a6f82b2bebbc"
+  end
 
 
   def install
@@ -16,6 +27,6 @@ class Ret < Formula
 
   test do
     output = shell_output("#{bin}/ret --version")
-    assert_match "0.2.2", output
+    assert_match "0.2.3", output
   end
 end
